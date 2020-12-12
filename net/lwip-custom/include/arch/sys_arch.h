a3b685da84ae9f4f1e2dae974ce3b44e742a3bda
tiqwab <tiqwab.ch90@gmail.com>
2020-12-12 18:03:00 +0900

    Add necessary macros for semaphore and mailbox
    ref. `contrib/ports/unix/port/include/arch/sys_arch.h` of lwip project

--- net/lwip-custom/include/arch/sys_arch.h
+++ net/lwip-custom/include/arch/sys_arch.h
@@ -3,9 +3,17 @@
 
 struct sys_sem_entry;
 typedef struct sys_sem_entry *sys_sem_t;
+#define sys_sem_valid(sem) (((sem) != NULL) && (*(sem) != NULL))
+#define sys_sem_valid_val(sem) ((sem) != NULL)
+#define sys_sem_set_invalid(sem) do { if((sem) != NULL) { *(sem) = NULL; }}while(0)
+#define sys_sem_set_invalid_val(sem) do { (sem) = NULL; }while(0)
 
 struct sys_mbox_entry;
 typedef struct sys_mbox_entry *sys_mbox_t;
+#define sys_mbox_valid(mbox) sys_sem_valid(mbox)
+#define sys_mbox_valid_val(mbox) sys_sem_valid_val(mbox)
+#define sys_mbox_set_invalid(mbox) sys_sem_set_invalid(mbox)
+#define sys_mbox_set_invalid_val(mbox) sys_sem_set_invalid_val(mbox)
 
 typedef int sys_thread_t;
 typedef int sys_mutex_t;
@@ -22,5 +30,9 @@ void lwip_core_init(void);
 #define SYS_ARCH_UNPROTECT(lev)
 
 #define SYS_ARCH_NOWAIT 0xfffffffe
+
+// FIXME: there two definitions are necessary?
+// #define sys_thread_new(n,t,a,s,p)
+// #define sys_msleep(t)
 
 #endif /* ifndef XV6_X86_64_ARCH_SYS_ARCH_H */
